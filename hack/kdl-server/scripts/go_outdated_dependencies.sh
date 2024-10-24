bf19d7999c39541153087bfb1fc418488197edab
ialejandro <ivan.alejandro@intelygenz.com>
2024-10-25 00:55:58 +0200

    chore(hack): remove scripts [skip ci]

--- hack/kdl-server/scripts/go_outdated_dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# https://github.com/golang/go/wiki/Modules#how-to-upgrade-and-downgrade-dependencies
-
-go list -u -f '{{if (and (not (or .Main .Indirect)) .Update)}}{{.Path}}: {{.Version}} -> {{.Update.Version}}{{end}}' -m all 2> /dev/null
